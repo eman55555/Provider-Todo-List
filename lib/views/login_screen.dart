2c933e6b35ef22e3a56d608065dd954d4765b69e
Eman Hamad <45310369+eman55555@users.noreply.github.com>
2023-11-07 04:35:32 +0200

    hive commit

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
-
 import '../constants/colors.dart';
 import 'components/text_button.dart';
 import 'components/text_form_field.dart';
